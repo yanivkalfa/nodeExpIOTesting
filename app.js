e061e6c87c14ad99488ad74376b55e44efaedd5a
Yaniv-Kalfa <netameta1@yahoo.com>
2014-09-14 16:59:01 -0700

    a bloody comment

--- app.js
+++ app.js
@@ -33,4 +33,46 @@ var s = {
         }
     }
 };
-s.oFns.init();
+//s.oFns.init();
+
+
+
+var oReq = require('./lib/requireFiles.js');
+
+var oRouts = require('./lib/requireRouts.js');
+
+
+oReq.app = oReq.express();
+oReq.http = oReq.http.Server(oReq.app);
+oReq.io = oReq.io(oReq.http);
+
+
+oReq.app.use(oReq.bodyParser.json());
+oReq.app.use(oReq.express.static(__dirname + '/css'));
+oReq.app.use(oReq.express.static(__dirname + '/js'));
+
+
+oReq.app.set('views', __dirname + '/tpl');
+oReq.app.set('view engine', "jade");
+oReq.app.engine('jade', oReq.jade.__express);
+
+
+oReq.app.get("/", function(req, res){
+    /*
+     res.locals.options = {
+     "chGlobal" : {
+     "rooms" : [s.oGlobal.rooms[0]],
+     "totalGames" : s.oGlobal.totalGames,
+     "totalMembers" : s.oGlobal.totalMembers,
+     "gameList" : s.oGlobal.gameList
+     },
+     "ajaxURL" : "/ajaxHandler",
+     "data" : {}
+     };
+     */
+    res.render('main');
+});
+
+oReq.http.listen(8000, function(){
+    console.log('listening on *:8000');
+});
