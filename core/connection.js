cd8d0f97d02680136c705adaaa35ee16d9639f31
Yaniv-Kalfa <netameta1@yahoo.com>
2014-09-14 19:03:36 -0700

    a bloody comment

--- core/connection.js
+++ core/connection.js
@@ -5,12 +5,6 @@ module.exports = function(_s){
     switch(con.adapter){
         case'mongoose':
             var url = 'aamongodb://' + con.user + ':' + con.password + '@' + con.host + ':' + con.port + '/' + con.database;
-
-            //var url = 'mongodb://testDB:abc123@54.86.187.241:27017/testDB';
-
-            console.log(url);
-            //console.log(_s.oReq.mongoose);
-
             var connect = function(count){
                 if(typeof count === 'undefined' || count < 0){
                     count = 0;
@@ -27,8 +21,6 @@ module.exports = function(_s){
             };
 
             connect();
-
-            //console.log(connection);
             break;
         case'otherAdapter':
 
