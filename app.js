fc00f2585bb4664fdde998f47386287332e7c085
Yaniv-Kalfa <netameta1@yahoo.com>
2014-09-14 17:34:18 -0700

    a bloody comment

--- app.js
+++ app.js
@@ -1,34 +1,19 @@
+var oReq = require('./lib/requireFiles.js')(),
+    oRouts = require('./lib/requireRouts.js')(),
+    oConfig = require('./config'),
+    oCore = require('./core'),
+    oDirname = __dirname,
+    oServerN = process.argv[2];
 
-/*  Stands for Server */
-global._s = {};
 
-global._s = {
-    oReq : require('./lib/requireFiles.js')(),
-    oRouts : require('./lib/requireRouts.js')(),
-    oConfig : require('./config'),
-    oCore : require('./core'),
-    oDirname : __dirname,
-    oServerN : process.argv[2],
-    //oGlobal : require('./lib/serverGlobal.js'),
-    //oConfig : require('./lib/serverConfig.js'),
-    //utilFunc : require('./lib/utilFunc.js'),
-    oFns : {
-        init : function(){
-            //console.log(s.oConfig);
-            //console.log(s.oCore);
-            //s.utilFunc.s = s;
+
+oReq.http.listen(8000, function(){
+    console.log('listening on *:8000');
+});
 
 
 
 
-
-            //s.oRouts(s);
-
-
-            this.oReq.http.listen(8000, function(){
-                console.log('listening on *:8000');
-            });
-        }
-    }
-};
-_s.oFns.init();
+//oGlobal : require('./lib/serverGlobal.js'),
+//oConfig : require('./lib/serverConfig.js'),
+//utilFunc : require('./lib/utilFunc.js'),
