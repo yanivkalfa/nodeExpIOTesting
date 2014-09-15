ac7296fd427f73b66871a3e2617bd8c225594aca
Yaniv-Kalfa <netameta1@yahoo.com>
2014-09-14 17:36:54 -0700

    a bloody comment

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-var oReq = require('./lib/requireFiles.js')(),
-    oRouts = require('./lib/requireRouts.js')(),
-    oConfig = require('./config'),
-    oCore = require('./core'),
-    oDirname = __dirname,
-    oServerN = process.argv[2];
+global._s.oReq = require('./lib/requireFiles.js')();
+global._s.oRouts = require('./lib/requireRouts.js')();
+global._s.oConfig = require('./config');
+global._s.oCore = require('./core');
+global._s.oDirname = __dirname;
+global._s.oServerN = process.argv[2];
 
 
 
-oReq.http.listen(8000, function(){
+
+_s.oReq.http.listen(8000, function(){
     console.log('listening on *:8000');
 });
 
