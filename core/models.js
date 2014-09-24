f9882947a3adf92041cb5cebcbb6f5de31d066eb
Yaniv-Kalfa <netameta1@yahoo.com>
2014-09-24 18:48:17 -0400

    a bloody comment

--- core/models.js
+++ core/models.js
@@ -2,7 +2,7 @@ module.exports = function(_s){
     var models = require('../models'),modelsSchema, modelName, schema, model = [];
 
     for(modelName in models){
-        console.log(modelName);
+        //console.log(modelName);
         schema = new _s.oReq.mongoose.Schema(models[modelName].schema);
         global[modelName] = oCore._connection.model(modelName, schema);
         model.push(global[modelName]);
