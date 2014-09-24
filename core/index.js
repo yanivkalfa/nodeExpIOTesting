45f7530069ab71b5892562bdc3e6ac5c992f03f8
Yaniv-Kalfa <netameta1@yahoo.com>
2014-09-24 18:55:38 -0400

    a bloody comment

--- core/index.js
+++ core/index.js
@@ -1,2 +1,11 @@
-module.exports._connection = function(_s){ return require('./connection.js')(_s); };
-module.exports._models = function(_s){ return require('./models.js')(_s);};
+module.exports = function(_s){
+    var _connection, _models;
+
+    _connection = require('./connection.js')(_s);
+    _models = require('./models.js')(_s);
+
+    return {
+        _connection : _connection,
+        _models : _models
+    }
+};
