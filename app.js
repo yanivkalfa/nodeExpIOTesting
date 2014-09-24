0f1889410f3a97c86a17410142ad615f49494c71
Yaniv-Kalfa <netameta1@yahoo.com>
2014-09-24 18:47:10 -0400

    a bloody comment

--- app.js
+++ app.js
@@ -38,6 +38,8 @@ app.use(_s.oReq.session({
 
 _s.oRouts = require('./lib/requireRouts.js')(_s);
 
+
+console.log(User);
 primus.on('connection', function (spark) {
     console.log('req', spark.request.session);
     console.log('connected', spark.id);
--- core/models.js
+++ core/models.js
@@ -1,6 +1,12 @@
 module.exports = function(_s){
-    var models = require('../models'),modelsSchema;
-    console.log(models);
+    var models = require('../models'),modelsSchema, modelName, schema, model = [];
 
-    return modelsSchema;
+    for(modelName in models){
+        console.log(modelName);
+        schema = new _s.oReq.mongoose.Schema(models[modelName].schema);
+        global[modelName] = oCore._connection.model(modelName, schema);
+        model.push(global[modelName]);
+    }
+
+    return model;
 };
