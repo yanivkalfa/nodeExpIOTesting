6e2b9d4bdaeb669a85461c6c28a6c986e4ce2a3f
Yaniv-Kalfa <netameta1@yahoo.com>
2014-09-26 18:41:36 -0400

    a bloody comment

--- app.js
+++ app.js
@@ -69,6 +69,10 @@ primus.on('connection', function (spark) {
         // send message to all clients except this one
         spark.room("aRoomName").except(spark.id).write(spark.id + ' joined room ' + "aRoomName");
 
+        primus.rooms(function(err, rooms){
+            console.log("primus room", rooms);
+        });
+
         primus.rooms(spark.id,function(err, rooms){
             console.log("primus room", rooms);
         });
